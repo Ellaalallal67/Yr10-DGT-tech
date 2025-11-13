cc860b68611b657c055d3c598814f2881d487919
6"7 nonchalant dreadhead feminist <ella.zhang@kingsway.school.nz>
2025-11-14 10:23:18 +1300

    Add files via upload

---
+++ area&perimeter calc2.py
@@ -0,0 +1,61 @@
+# Date: 14-11-2025
+# Author: Ella Zhang
+# YR10 DGT 
+# Version 3.13.7
+#Calculates the area and perimeter for a shape 
+
+#Loop
+def num_check():
+    keep_looping =""
+    while keep_looping =="":
+        #Main error msg
+        error = "please enter a number more than 0. \n" 
+        #Error msg for when the user inputs something that is not a valid number
+        error2 = "Please enter a number."
+       
+        #Asks the user for width
+        while True:
+            try:
+                width = float(input("Enter your width: "))
+                #Error checking for width
+                if width > 0:
+                     print(f"Your width is {width}")
+                     break
+                else:
+                     print(error)
+            except ValueError:
+                    print(error2)
+
+        #Asks user for height          
+        while True:
+            try:  
+                height = float(input("Enter your height: ")) 
+                #Error checking for height
+                if height > 0:
+                    print(f"Your height is {height}")
+                    break
+                else:
+                     print(error)
+            except ValueError:
+                print(error2)
+                           
+            #Formulas to calculate the perimeter and area
+            perimeter = 2*(width + height)
+            area = width*height
+                    
+            #print answer
+            print()
+            print(f"perimeter: {perimeter} units; area: {area} square units. \n")
+            #Ask user if they want to run the program again 
+            loop = input("Press <ENTER> to continue, or any other key to quit.")
+            loop2 = loop
+            #Error checking
+                
+            if height < 0 and height == 0:
+                print(error)
+            #Loop stops
+            if loop2 != loop:
+                continue
+            else:
+                keep_looping = "stop_loop"
+num_check()
